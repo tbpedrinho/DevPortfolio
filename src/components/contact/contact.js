a5d9447bd28342b4566090963815e3b641cce5e9
tbpedrinho <phtbpedrinho@gmail.com>
2019-09-02 14:22:36 -0300

    Adicionando ícones e links de contato

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,7 +4,11 @@ import React from "react";
 import { Row, Col, Card, Input, Button } from 'react-materialize';
 // Importando o componenet UserProfile
 import UserProfile from '../user_profile/user_profile';
-
+//Importando a imagem das redes sociais
+import faceicone from '../../images/faceicone.png';
+import instaicone from '../../images/instaicone.png';
+import wppicone from '../../images/wppicone.png';
+import giticon from '../../images/giticon.png';
 const Contact = () => (
   <Row>
     <Col m={3} s={12}>
@@ -14,10 +18,18 @@ const Contact = () => (
         <h5>Contact</h5>
         <Card>
           <Row>
-              <Input placeholder="phtbpedrinho" type="email" label="Email" s={12} />
-              <Input placeholder="Assunto" label="Message" s={12} />
-            <Col s={12} m={12}>
-              <Button waves='light' className="right grey darken-2">SEND</Button>
+              <Input placeholder="E-mail" type="email" label="Email" s={12} />
+              <Input placeholder="Assunto" label="Mensagem" s={12} />
+            <Col s={8} m={8}> 
+              <tr>
+              <td><a href="https://www.facebook.com/pedrohenrique.teixeirabraga"><img src={faceicone} class="img-circle" alt="Responsive Image" width="40" height="40"/></a></td>
+              <td><a href="https://www.instagram.com/tbpedrinho"><img src={instaicone} class="img-circle" alt="Responsive Image" width="40" height="40"/></a></td>
+              <td><a href="http://api.whatsapp.com/send?1=pt_BR&phone=5521972806876"><img src={wppicone} class="img-circle" alt="Responsive Image" width="40" height="40"/></a></td>
+              <td><a href="https://github.com/tbpedrinho"><img src={giticon} class="img-circle" alt="Responsive Image" width="40" height="40"/></a></td>
+              </tr>
+            </Col>
+            <Col s={1} m={1}>
+            <th><Button waves='light' className="right grey darken-2">SEND</Button></th>
             </Col>
           </Row>
         </Card>
